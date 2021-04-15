59ebf15db2580be18fd58e7adef9aec32401a6f4
Nutchanokchon Pooyatorn <nutchanokchon5@gmail.com>
2021-04-15 23:47:32 +0700

    done

--- app.js
+++ app.js
@@ -1,42 +1,14 @@
 const items = document.querySelector(".lists");
-const searchBar = document.querySelector("#search");
+const searchBar = document.querySelector(".search");
 
-let users = []
+
 
 
 //fetch api to get data and assign to variable "trips"
-async function fetchTrip(){
+/*async function fetchTrip(){
     const response = await fetch("https://trips-api-gateway.herokuapp.com/trips");
     const trips = await response.json();
      showTrips(trips)
-
-
-
-     //filterable function
-    searchBar.addEventListener("input", e => {
-    const element = e.target.value
-    const newTrip = []
-    
-    trips.filter(trip => {
-        if(trip.title.includes(element)){
-            newTrip.push(trip.title)
-            showTrips(newTrip) 
-        }else if(trip.tags.includes(element)){
-            newTrip.push(trip.tags)
-            showTrips(newTrip) 
-        }else if(trip.description.includes(element)){
-            newTrip.push(trip.description)
-            showTrips(newTrip) 
-        }else{
-            console.log('no matched trip')
-        }
-        
-    })
-   
- 
-})
-
-
 }
 fetchTrip()
 .then(response => {
@@ -45,41 +17,77 @@ fetchTrip()
 .catch(error => {
     console.log('error');
     console.log(error);
+})*/
+
+
+let  searchTrips = [];
+
+searchBar.addEventListener("keyup", e => {
+    const searchString = e.target.value;
+    const filteredTrips =  searchTrips.filter(trip => {
+    return (
+        trip.title.includes(searchString) ||
+        trip.tags.includes(searchString) ||
+        trip.description.includes(searchString)
+    );
+});
+    showTrips(filteredTrips);
+    console.log(filteredTrips)
 })
 
 
 
-//make the list filterable
 
 
 
 
-const showTrips = arr => {
-    let output = "";
 
-    arr.forEach(({ tags, photos, title, description, url}) => 
-    (output += `<li class="container-list">
+
+
+const loadTrips = async() => {
+    try{
+        const res = await fetch("https://trips-api-gateway.herokuapp.com/trips");
+        const searchTrips = await res.json();
+        showTrips(searchTrips);
+    }catch(err){
+        console.log(err);
+    }
+};
+
+
+
+
+
+
+
+
+const showTrips = (trips) => {
+    const output = trips
+        .map((trip) => {
+        
+        return ` 
+            <li class="container-list">
        <div class="image-card">
-           <img src=${photos[0]} alt="" />
+           <img src=${trip.photos[0]} alt="" />
        </div>
 
        <div class="contents">
            <div class="content">
                <div class="title">
-                   <h2>${title}</h2>
+                   <h2>${trip.title}</h2>
                </div>
                
                <div class="description" id="module">
-                   <p class="collapse" id="collapseId" aria-expanded="false">${description}</p>
-                   <a role="button" class="collapsed" data-toggle="collapse" href="${url}" aria-expanded="false"> 
+                   <p class="collapse" id="collapseId" aria-expanded="false">${trip.description}</p>
+                   <a role="button" class="collapsed" data-toggle="collapse" href="${trip.url}" aria-expanded="false"> 
                     </a>
                 </div>
            
-               <div class="hashtags">
+               <div class="hashtags" id="hashtags">
                    <h5>หมวดหมู่: </h5>
                    <span class="tag" id="dots">
-                       <p class="collapse_" id="collapseTag" aria-expanded="false"> ${tags}</p>
-                       <a role="button" class="collapseDot" data-toggle="collapse_" href="${url}" aria-expanded="false" > 
+                       <p class="collapse_" id="collapseTag" aria-expanded="false"> ${trip.tags}</p>
+                       <a role="button" class="collapseDot" data-toggle="collapse_" href="${trip.url}" aria-expanded="false" > 
                    </span>
                </div>
               
@@ -87,23 +95,58 @@ const showTrips = arr => {
                <div class="images-slide">
             
                    <div class="slide">
-                       <img src=${photos[1]} />
-                       <img src=${photos[2]} />
-                       <img src=${photos[3]} />
+                       <img src=${trip.photos[1]} />
+                       <img src=${trip.photos[2]} />
+                       <img src=${trip.photos[3]} />
                     </div>  
                </div>
             </div> 
        </div>
    </li>`
-    )
-    )
-    items.innerHTML = output
+        }).join('');
     
+    items.innerHTML = output;
+    
+
 }
 
-
-document.addEventListener("DOMContentLoaded", fetchTrip);
-
+loadTrips();
 
 
+
+
+// function myFunction(){
+
+//     let input, filter, element, span, p, tag, i;
+//     input = document.getElementById("search");
+
+//     filter = input.value.toUpperCase();
+  
+//     //tag_div = document.getElementById("hashtags");
+//     //console.log(tag_div)
+//     // element = document.querySelector(".hashtags");
+//     // console.log(element)
+//     // span=  element.querySelector(".tag"); 
+//     // console.log(span)
+//     // p = span.getElementByClassName("collapse_");
+
+//     p = document.getElementsByClassName("collapse_");
+ 
+
+
+//     for( i=0; i< p.lenght; i++){
+//             tag = p[i]
+          
+//        if( tag.innerHTML.toUpperCase().indexOf(filter)===0){
+//             p[i].style.display = "";
+//             console.log(tag)
+//        }else{
+//            p[i].style.display = "none";
+//  console.log(tag)
+//        }
+   
+//     }
+
+
+// }
 
